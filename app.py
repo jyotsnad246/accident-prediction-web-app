731e60ca8676891fecf2880631d5245c98818cbb
jyotsnad246 <jyotsnay24@gmail.com>
2023-07-09 11:32:05 +0530

    updating app.py

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 
 import streamlit as st
 import pandas as pd
-import pickle
+#import pickle
+import tensorflow as tf
 import numpy as np
 from PIL import Image
 import os
@@ -25,8 +26,9 @@ def main():
 
     st.markdown("""
                 The app forecasts the number of accidents in a month based on the **“Monatszahlen Verkehrsunfälle”** Dataset from the München Open Data Portal.  
-                [Link to the dataset](https://www.opengov-muenchen.de/dataset/monatszahlen-verkehrsunfaelle/resource/40094bd6-f82d-4979-949b-26c8dc00b9a7)
             """)
+    st.write("[Link to the Dataset](https://opendata.muenchen.de/dataset/monatszahlen-verkehrsunfaelle/resource/40094bd6-f82d-4979-949b-26c8dc00b9a7)")
+
     st.markdown("---")
 
     # Sidebar for input values
@@ -56,27 +58,35 @@ def main():
     input_data = [[en_cat, en_type, int(year), int(options_for_month)]]
     input_data = np.array(input_data)
 
-    model_path = os.path.join(current_dir, "models", "mymodel.pkl")
-
+    #model_path = os.path.join(current_dir, "models", "mymodel.pkl")
+    model_path = os.path.join(current_dir, "models", "mymodel.h5")
     # Loading the model
-    with open(model_path, 'rb') as f:
-        model = pickle.load(f)
-
+    #with open(model_path, 'rb') as f:
+    #    model = pickle.load(f)
+    
+    model = tf.keras.models.load_model(model_path)
 
     # Prediction section
     if st.sidebar.button("Forecast"):
         prediction = model.predict(input_data)
         st.success('The forecasted number of accidents is: **{}**'.format(int(prediction[0])))
+        st.write("This result is generated using a GRU Netural Network Model")
         st.sidebar.markdown("---")
 
     # Display Future Forecast Trends
-    st.subheader("Future Forecast Graph Generated Using Prophet")
+    st.subheader("Future Forecast of Total Accidents - Generated Using Prophet")
     if en_cat == 0:
+        st.write("For Alkoholunfälle")
         st.image(Image.open('images/prophetAlkoholunfälle.png'))
     elif en_cat == 1:
+        st.write("For Fluchtunfälle")
         st.image(Image.open('images/prophetFluchtunfälle.png'))
     else:
+        st.write("For Verkehrsunfälle")
         st.image(Image.open('images/prophetVerkehrsunfälle.png'))
+
+    
+    st.write("[GitHub Code Link](https://github.com/jyotsnad246/accident-prediction-web-app)")
 
 
 if __name__ == '__main__':
